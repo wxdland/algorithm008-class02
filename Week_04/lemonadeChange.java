34b610b0f8b8a61ff3285e3bfedab045d6341fdf
wuxd <wuxd@epshealth.com>
2020-05-15 23:41:01 +0800

    week4-day5

---
+++ Week_04/lemonadeChange.java
@@ -0,0 +1,53 @@
+package 第四周;
+
+/*
+在柠檬水摊上，每一杯柠檬水的售价为 5 美元。
+顾客排队购买你的产品，（按账单 bills 支付的顺序）一次购买一杯。
+每位顾客只买一杯柠檬水，然后向你付 5 美元、10 美元或 20 美元。你必须给每个顾客正确找零，也就是说净交易是每位顾客向你支付 5 美元。
+注意，一开始你手头没有任何零钱。
+如果你能给每位顾客正确找零，返回 true ，否则返回 false 。
+
+示例 1：
+输入：[5,5,5,10,20]
+输出：true
+解释：
+前 3 位顾客那里，我们按顺序收取 3 张 5 美元的钞票。
+第 4 位顾客那里，我们收取一张 10 美元的钞票，并返还 5 美元。
+第 5 位顾客那里，我们找还一张 10 美元的钞票和一张 5 美元的钞票。
+由于所有客户都得到了正确的找零，所以我们输出 true。
+date:2020-5-15 23:07
+*/
+public class lemonadeChange {
+
+  public static boolean lemonadeChange(int[] bills) {
+    int five = 0, ten = 0, tewnty = 0;
+    for (int bill : bills) {
+      if (bill == 5) {
+        five++;
+      } else if (bill == 10) {
+        ten++;
+        five--;
+      } else {
+        tewnty++;
+        if (ten > 0) {
+          ten--;
+        } else {
+          five--;
+          five--;
+        }
+        five--;
+      }
+    }
+    //如果顾客给20元，需要找10元，5元各一张，这时如果手上没现金就five，ten就是-1；
+    if (five < 0 || ten < 0) {
+      return false;
+    }
+    return true;
+  }
+
+
+  public static void main(String[] args) {
+    boolean tt=lemonadeChange(new int[]{5,5,10,10,20});
+    System.out.println(tt);
+  }
+}
